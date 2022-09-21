d1c8492343933e3b3bf9e13c695d9205775cf30e
hdXian <hyun353500@naver.com>
2022-09-21 23:27:28 +0900

    0922 commit

---
+++ src/test/java/ctoy2work/example/tistorycomment/TistoryCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package ctoy2work.example.tistorycomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
